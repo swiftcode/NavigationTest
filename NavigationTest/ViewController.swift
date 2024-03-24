f6947b8de074fe4594cfa36050acf6b2feda864c
swiftcode <swiftcode@users.noreply.github.com>
2024-03-24 06:23:55 -0500

    Initial Commit

---
+++ NavigationTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationTest
+//
+//  Created by Michael Campbell on 3/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
